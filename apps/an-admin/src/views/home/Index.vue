ffa44df5b3abb94f7649aafff8a6ce162afeef65
wangjunzjhw <percy7@yeah.net>
2022-03-04 17:56:02 +0800

    目录组件

--- apps/an-admin/src/views/home/Index.vue
+++ apps/an-admin/src/views/home/Index.vue
@@ -1,17 +1,31 @@
 <template lang="pug">
   div.home
     div {{ title }}
+    BaseTree(:treeConfig="treeConfig", @onChangeCurrentNode="onChangeCurrentNode")
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import BaseTree from "@/an-admin/src/components/base-tree/BaseTree.vue"
 
 @Component({
-	components: {}
+	components: {
+    BaseTree
+  }
 })
 export default class Home extends Vue {
 
   title = "后台"
+  treeConfig = {
+    isSearch: true,
+    mode: "read",
+    grade: 2
+  }
+
+  // 当前选中的节点
+  onChangeCurrentNode(value) {
+    console.log("当前选中节点", value);
+  }
 }
 </script>
 
